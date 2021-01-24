d5bac288804f0cdcd13a9248473d45b2cb77807a
DJTai <DJTai@pm.me>
2021-01-23 19:10:35 -0800

    Lint with flake8

--- ancient-chamorro-society/acs_scraper.py
+++ ancient-chamorro-society/acs_scraper.py
@@ -2,17 +2,17 @@ import json
 import logging
 import os
 import requests
-import time
 import urllib.request
 
 from bs4 import BeautifulSoup as bs
+
 
 def main():
     main_url = 'https://ancientchamorrosociety.weebly.com/'
     json_file = 'ancient_chamorro_society.json'
     cwd = os.getcwd()
-    
-    ## Create JSON of JPEG URLs for future reference
+
+    # Create JSON of JPEG URLs for future reference
     hrefs = []  # ex: /chapter-1.html
     pages = {}
 
@@ -29,7 +29,7 @@ def main():
             key = link.get('href')[1:].split('.')[0]
             pages[key] = {}
             pages[key]['href'] = href
-            pages[key]['images'] = []            
+            pages[key]['images'] = []
 
     # Create BeautifulSoup objs for all hrefs/chapters
     logging.info("Going through each Chapter HTML and getting image URLs")
@@ -60,7 +60,7 @@ def main():
     with open(json_file, 'w') as my_file:
         json.dump(pages, my_file, indent=4)
 
-    ## DOWNLOAD IMAGES
+    # DOWNLOAD IMAGES
     img_dir = 'acs_images'
     img_wd = cwd + '\\acs_images\\'
     logging.info("Creating %s", img_dir)
@@ -69,9 +69,9 @@ def main():
     logging.info("Load JSON")
     with open(json_file, 'r') as myfile:
         data = myfile.read()
-    
+
     loaded_pages = json.loads(data)
-    
+
     logging.info("Downloading JPEGs")
     for key in loaded_pages:    # ex: chapter-1
         os.chdir(img_wd)        # cd cwd\\acs_images\\
@@ -85,7 +85,7 @@ def main():
         for i in range(0, len(images)):
             img_name = images[i].split('/')[-1]  # ex: filename_orig.jpg
             img_name = str(i).zfill(3) + '_' + img_name
-            
+
             urllib.request.urlretrieve(images[i], img_name)
 
 
